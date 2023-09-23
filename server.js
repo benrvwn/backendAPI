bc267e440a488a89d47605e08206cd40ee59c602
benvrwn <binverwin16@gmail.com>
2023-09-23 15:13:22 +0800

    ewqewq

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('response to hahaha');
 });
-app.get('/gagisupot', (req, res) => {
-    res.send('testing kung gumaganaa');
+
+
+app.post('/addProduct', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
 });
 
 mongoose.set('strictQuery', false);
